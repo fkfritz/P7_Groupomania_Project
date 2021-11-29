f6e69d315c29e148ac7555ced3e262772745db17
Fritznel Franck <univfranck@gmail.com>
2021-11-29 11:12:38 -0300

    Create middleware auth

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const dotenv = require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, `${process.env.JWT_RAND_SECRET}`);
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "User Id non valide";
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(401).json({ error: error | "Requête non authentifiée !" });
+  }
+};
